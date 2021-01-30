1ece4d38d529f20b7ab9fadcfbe142f41fbaaf97
Adalberto Pereira <adalbertobosco@gmail.com>
2021-01-30 19:10:34 -0300

    icon added

--- personregister/ClientApp/src/app/get-id-person/get-id-person.component.ts
+++ personregister/ClientApp/src/app/get-id-person/get-id-person.component.ts
@@ -12,7 +12,7 @@ export class GetIdPersonComponent {
   id = 0;
   name = 'teste';
   cpf = 0;
-  birthdate = '';
+  birthDate = '';
   income: number = 0;
 
   constructor(public http: HttpClient, @Inject('BASE_URL') public baseUrl: string) {
@@ -22,7 +22,7 @@ export class GetIdPersonComponent {
     this.http.get<Person>(this.baseUrl + 'api/Person/'+this.id).subscribe(result => {
       this.name = result.name;
       this.cpf = result.cpf;
-      this.birthdate = result.birthdate;
+      this.birthDate = result.birthDate;
       this.income = result.income;
     }, error => console.error(error));
   }
@@ -32,7 +32,7 @@ export class GetIdPersonComponent {
     const person: Person = {
       name: this.name,
       cpf: this.cpf,
-      birthdate: this.birthdate,
+      birthDate: this.birthDate,
       income: this.income
     };
     this.http.put<Person>(this.baseUrl + 'api/Person/'+this.id, person).subscribe(result => {
--- personregister/ClientApp/src/app/person-crud/person-crud.component.ts
+++ personregister/ClientApp/src/app/person-crud/person-crud.component.ts
@@ -6,12 +6,21 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './person-crud.component.html',
   styleUrls: ['./person-crud.component.css']
 })
-export class PersonCRUDComponent {
+export class PersonCRUDComponent  {
   public persons: Person[];
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+  constructor(public http: HttpClient, @Inject('BASE_URL') public baseUrl: string) {
+    this.GetPersons(http, baseUrl);
+  }
+  private GetPersons(http: HttpClient, baseUrl: string) {
     http.get<Person[]>(baseUrl + 'api/Person').subscribe(result => {
       this.persons = result;
+    }, error => console.error(error));
+  }
+
+  delete(id: number) {
+    this.http.delete<Person>(this.baseUrl + 'api/Person/'+id).subscribe(result => {
+      this.GetPersons(this.http,this.baseUrl);
     }, error => console.error(error));
   }
 }
