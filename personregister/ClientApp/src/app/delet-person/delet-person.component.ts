4617acaae91b7f2810da63fc7454d73d3ae76304
Adalberto Pereira <adalbertobosco@gmail.com>
2021-01-30 18:29:44 -0300

    delete working =D

--- personregister/ClientApp/src/app/delet-person/delet-person.component.ts
+++ personregister/ClientApp/src/app/delet-person/delet-person.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { Person } from '../person-crud/person-crud.component';
 
 @Component({
   selector: 'app-delet-person',
   templateUrl: './delet-person.component.html',
   styleUrls: ['./delet-person.component.css']
 })
-export class DeletPersonComponent implements OnInit {
+export class DeletPersonComponent {
+  id = 0;
+  name = 'teste';
+  cpf = 0;
+  birthdate = '';
+  income: number = 0;
 
-  constructor() { }
+  constructor(public http: HttpClient, @Inject('BASE_URL') public baseUrl: string) { }
 
-  ngOnInit() {
+  getId() {
+    this.http.get<Person>(this.baseUrl + 'api/Person/'+this.id).subscribe(result => {
+      this.name = result.name;
+      this.cpf = result.cpf;
+      this.birthdate = result.birthdate;
+      this.income = result.income;
+    }, error => console.error(error));
   }
-
+  delete() {
+    this.http.delete<Person>(this.baseUrl + 'api/Person/'+this.id).subscribe(result => {
+    }, error => console.error(error));
+  }
 }
